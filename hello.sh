1a8a2e52c16d7cdf753593908f57a8ab5105ab5b
luiscal4a <luiscal4a@gmail.com>
2020-10-15 20:21:42 +0200

    Added bash file with instructions

---
+++ hello.sh
@@ -0,0 +1,8 @@
+echo Este es mi primer script
+echo Quieres una taza de chocolate?
+echo Pulsa S para si
+echo Pulsa N para no
+read -r -p " [Y/n] " input
+clear
+echo Screen cleared
+echo "$(date)"
