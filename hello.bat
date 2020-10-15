6bf2a109fdf4e6b0d6f70176310ade6372843f1a
luiscal4a <luiscal4a@gmail.com>
2020-10-15 20:13:54 +0200

    Added bat file with instructions

---
+++ hello.bat
@@ -0,0 +1,11 @@
+@echo off
+ECHO Este es mi primer script
+ECHO Quieres una taza de chocolate?
+ECHO Pulsa S para si
+ECHO Pulsa N para no
+CHOICE /c SN /m "Si No"
+CLS
+ECHO Screen cleared
+ECHO %DATE%
+ECHO %TIME%
+pause
